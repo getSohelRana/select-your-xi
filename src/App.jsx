56744ba58a9ccad4ba06db0004170a8903922a97
getSohelRana <getsohelinfo@gmail.com>
2025-09-25 00:07:58 +0600

    json data loaded and show each cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,25 @@
 
 import './App.css'
-import navImg from "./assets/logo.png"
-import dollarIcon from "./assets/dollar.png"
+import Navbar from './components/Navbar/Navbar'
+import AvailablePlayers from './components/Navbar/AvailablePlayers/AvailablePlayers'
+import SelectedPlayers from './components/Navbar/SelectedPlayers/SelectedPlayers'
+import { Suspense } from 'react'
+
+const fetchPlayer = async () => {
+  const res = await fetch('./player.json')
+  return res.json()
+}
+
 function App() {
   
-
+  const palyerPromise = fetchPlayer()
   return (
     <>
-      <div className="navbar">
-        <div className="navbar-start">
-          <div className="dropdown">
-            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-            </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content  rounded-box z-1 mt-3 w-52 p-2 shadow">
-              <li><a>Home</a></li>
-              <li><a>Fixture</a></li>
-              <li><a>Teams</a></li>
-              <li><a>Schedules</a></li>
-            </ul>
-          </div>
-          <a className="text-xl">
-            <img src={navImg} className='w-[60px] object-cover' alt="site logo" />
-          </a>
-        </div>
-        <div className="navbar-end hidden lg:flex">
-          <ul className="menu menu-horizontal px-1 text-xl text-gray-600">
-            <li><a>Home</a></li>
-            <li><a>Fixture</a></li>
-            <li><a>Teams</a></li>
-            <li><a>Schedules</a></li>
-          </ul>
-        </div>
-        <div className="navbar-end">
-          <a className="btn bg-white shadow-none text-black border-1 border-gray-100">
-            <span>600000</span>
-            <span>Coin</span>
-            <img className='w-[20px] object-cover' src={dollarIcon} alt="" />
-          </a>
-        </div>
-      </div>
+      <Navbar></Navbar>
+      <Suspense fallback = {<span className="loading loading-spinner loading-xl "></span>}>
+        <AvailablePlayers palyerPromise = {palyerPromise}></AvailablePlayers>
+      </Suspense>
+      {/* <SelectedPlayers></SelectedPlayers> */}
     </>
   )
 }
---
+++ src/components/Navbar/SelectedPlayers/SelectedPlayers.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SelectedPlayers = () => {
+    return (
+        <div>
+            seleted players
+        </div>
+    );
+};
+
+export default SelectedPlayers;
